0936a9ad04c9f5f82d8aec86ef024eeb1ff02f0f
Theodore Felix Leo <tflhyl@gmail.com>
2014-08-04 15:40:56 +0800

    Make text center vertically.

--- JSBadgeView/JSBadgeView.m
+++ JSBadgeView/JSBadgeView.m
@@ -414,7 +414,7 @@ static BOOL JSBadgeViewIsUIKitFlatMode(void)
             const CGSize textSize = [self sizeOfTextForCurrentSettings];
             
             textFrame.size.height = textSize.height;
-            textFrame.origin.y = rectToDraw.origin.y + ceilf((rectToDraw.size.height - textFrame.size.height) / 2.0f);
+            textFrame.origin.y = rectToDraw.origin.y + ceilf((rectToDraw.size.height - textFrame.size.height) / 2.0f) - 0.5;
 
             JSBadgeViewSilenceDeprecatedMethodStart();
             [self.badgeText drawInRect:textFrame
